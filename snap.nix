840421f4b2271cd53fb2c09353666287f1883904
Valentin Robert <valentin.robert.42@gmail.com>
2016-06-17 08:19:15 -0700

    nix files

---
+++ snap.nix
@@ -0,0 +1,35 @@
+{ mkDerivation, aeson, async, attoparsec, base, bytestring, cereal
+, clientsession, configurator, containers, deepseq, directory
+, directory-tree, dlist, filepath, Glob, hashable, heist
+, http-streams, HUnit, lens, lifted-base, map-syntax, monad-control
+, mtl, mwc-random, pwstore-fast, QuickCheck, smallcheck, snap-core
+, snap-server, stdenv, stm, syb, test-framework
+, test-framework-hunit, test-framework-quickcheck2
+, test-framework-smallcheck, text, time, transformers
+, transformers-base, unordered-containers, xmlhtml
+}:
+mkDerivation {
+  pname = "snap";
+  version = "1.0.0.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    aeson attoparsec base bytestring cereal clientsession configurator
+    containers directory directory-tree dlist filepath hashable heist
+    lens lifted-base map-syntax monad-control mtl mwc-random
+    pwstore-fast snap-core snap-server stm text time transformers
+    transformers-base unordered-containers xmlhtml
+  ];
+  testHaskellDepends = [
+    aeson async attoparsec base bytestring cereal clientsession
+    configurator containers deepseq directory directory-tree dlist
+    filepath Glob hashable heist http-streams HUnit lens lifted-base
+    map-syntax monad-control mtl mwc-random pwstore-fast QuickCheck
+    smallcheck snap-core snap-server stm syb test-framework
+    test-framework-hunit test-framework-quickcheck2
+    test-framework-smallcheck text time transformers transformers-base
+    unordered-containers xmlhtml
+  ];
+  homepage = "http://snapframework.com/";
+  description = "Top-level package for the Snap Web Framework";
+  license = stdenv.lib.licenses.bsd3;
+}
