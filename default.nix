8232ec3bc150deee0ad56cd3e7c203099f75ae25
Valentin Robert <valentin.robert.42@gmail.com>
2016-06-17 09:20:55 -0700

    necessary changes

--- default.nix
+++ default.nix
@@ -1,5 +1,14 @@
 { nixpkgs ? import <nixpkgs> {}
 , compiler ? "ghc7103"
 }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./snap.nix { }
+let snap = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./snap.nix {}; in
+nixpkgs.lib.overrideDerivation snap (old:
+  { buildInputs = old.buildInputs ++ (with nixpkgs; [
+      zlib
+    ]);
+    nativeBuildInputs = old.nativeBuildInputs ++ (with nixpkgs; [
+      zlib
+    ]);
+  }
+)
 
