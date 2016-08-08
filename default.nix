601990bc9bfeaa4f83b8714f6b9be9d1c80bdc1d
Valentin Robert <valentin.robert.42@gmail.com>
2016-08-08 16:54:41 -0700

    don't specify compiler

--- default.nix
+++ default.nix
@@ -1,6 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}
-, compiler ? "ghc7103"
-}:
+{ nixpkgs ? import <nixpkgs> {}, compiler }:
 let snap = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./snap.nix {}; in
 nixpkgs.lib.overrideDerivation snap (old:
   { buildInputs = old.buildInputs ++ (with nixpkgs; [
